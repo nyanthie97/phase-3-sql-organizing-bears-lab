5af3d948bf3e27c8dc58e38e89ec69863478f259
Nyanthie Mayang <nyanthie.mayang@student.moringaschool.com>
2022-09-06 23:13:10 +0300

    commit tree 2475b3d46f884ec08ecbb497c4bd1bc6d324a1bf

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 7, 'M', 'brown', 'grumpy', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Roudy', 4, 'M', 'brown', 'playful', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 5, 'F', 'Brown', 'sleepy', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 12, 'F', 'brown', 'sleepy', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melisa', 6, 'F', 'black', 'hungry', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 4, 'M', 'black', 'playful', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 7, 'F', 'black', 'grumpy', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 5, 'M', 'black', NULL, false);
